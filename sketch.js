65d5d469d14ae95b78a3d49cdbb7c54abed3952d
Dan Li <dl4224@nyu.edu>
2020-03-10 12:45:57 +0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let slider1;
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    slider1=createSlider(0,255);
+}
+
+function draw(){
+    background(slider1.value(),0,0);
+    
+}
+
+
